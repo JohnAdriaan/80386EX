0259183aecdaab1406fdccc9215b399c2e268eb3
John Adriaan <John.Adriaan@BigPond.com>
2025-08-26 19:13:07 +1000

    Move Executive Device drivers into sub-directory, and enhance the Project comments

--- 80386EX.asm
+++ 80386EX.asm
@@ -21,14 +21,22 @@
 ; files to provide a single image that is produced by the assembler.
 ;
 ; However, modularity is not lost: in fact, most files `%include` their own
-; sub-files to make one cohesive whole. The project file hierarchy is designed
-; to make it easy to know where to go:
-; `x86`  : Definitions for the basic x86 CPU
+; sub-files to make one cohesive whole, separated into distinct directories.
+; The project file hierarchy is designed to make it easy to know where to go:
+; `x86`  : Definitions for the basic x86 CPU - and its variants
 ; `Dev`  : Definitions for devices
 ; 'Demo' : Let's see what we can do with Protected Mode
-; 'Boot' : Boot code
-; 'Boot/Real' : Real-mode boot code
-; 'Boor/Prot' : Protected-mode boot code
+; 'Demo/Ints' : More sophisticated Fault Handlers, IRQ handlers and User INTs
+; 'Demo/Ints/Debug' : A visual debugger to examine registers and memory
+; 'Demo/User' : The User-mode code run many times for multi-tasking
+; 'Demo/Exec' : The Executive that sets up and runs the User code
+; 'Demo/Exec/Ints' : The initialisation of the environment for the Fault Handlers
+; 'Demo/Exec/Dev'  : The initialisation of the peripherals, including any IRQ(s)
+; 'Demo/Exec/User' : The initialisation of the environment to run User code
+; 'Demo/Exec/Alloc' : The allocation routines for within System tables
+; 'Boot' : Boot code, to set up a minimalist environment for better code later
+; 'Boot/Real' : Real-mode boot code, just to get ready for Protected Mode
+; 'Boot/Prot' : Protected-mode boot code, to initialise peripherals
 ;===============================================================================
 
                 SECTALIGN         OFF   ; No implicit ALIGN => SECTALIGN effects
